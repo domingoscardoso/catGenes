bd15c3a3300a5c0fa81c2eca5fcfc6776e9e9f23
Domingos <cardosobot@gmail.com>
2021-01-12 23:07:04 -0300

    fix

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,7 +7,7 @@ library(ape)
 # Loading Luetzelburgia example
 genes <- list.files("data-raw/DNAlignments/Luetzelburgia")
 Luetzelburgia <- list()
-for (i in genes){
+for (i in genes) {
   Luetzelburgia[[i]] <- ape::read.nexus.data(paste0("data-raw/DNAlignments/Luetzelburgia/",i))
 }
 names(Luetzelburgia) <- gsub(".*_(.+)[.].*", "\\1", names(Luetzelburgia))
@@ -19,7 +19,7 @@ usethis::use_data(Luetzelburgia, overwrite = TRUE)
 # Loading Ormosia example
 genes <- list.files("data-raw/DNAlignments/Ormosia")
 Ormosia <- list()
-for (i in genes){
+for (i in genes) {
   Ormosia[[i]] <- ape::read.nexus.data(paste0("data-raw/DNAlignments/Ormosia/",i))
 }
 names(Ormosia) <- gsub(".*_(.+)[.].*", "\\1", names(Ormosia))
@@ -31,7 +31,7 @@ usethis::use_data(Ormosia, overwrite = TRUE)
 # Loading Vataireoids example
 genes <- list.files("data-raw/DNAlignments/Vataireoids")
 Vataireoids <- list()
-for (i in genes){
+for (i in genes) {
   Vataireoids[[i]] <- ape::read.nexus.data(paste0("data-raw/DNAlignments/Vataireoids/",i))
 }
 names(Vataireoids) <- gsub("[.].*", "", names(Vataireoids))
@@ -43,7 +43,7 @@ usethis::use_data(Vataireoids, overwrite = TRUE)
 # Loading Gaya example
 genes <- list.files("data-raw/DNAlignments/Gaya")
 Gaya <- list()
-for (i in genes){
+for (i in genes) {
   Gaya[[i]] <- ape::read.nexus.data(paste0("data-raw/DNAlignments/Gaya/",i))
 }
 names(Gaya) <- gsub("[.].*", "", names(Gaya))
@@ -55,10 +55,43 @@ usethis::use_data(Gaya, overwrite = TRUE)
 # Loading Brongniartia example
 genes <- list.files("data-raw/DNAlignments/Brongniartia")
 Brongniartia <- list()
-for (i in genes){
+for (i in genes) {
   Brongniartia[[i]] <- ape::read.nexus.data(paste0("data-raw/DNAlignments/Brongniartia/",i))
 }
 names(Brongniartia) <- gsub("[.].*", "", names(Brongniartia))
 
 # Adding dataset for tests
 usethis::use_data(Brongniartia, overwrite = TRUE)
+
+
+
+# Loading Vochysiaceae example
+# See that the original DNA alignments are in Phylip format.
+# So we will first import into R and them save in Nexus format
+genes <- list.files("data-raw/DNAlignments/Vochysiaceae")
+Vochysiaceae <- list()
+for (i in genes) {
+  chopper::alg2nex(paste0("data-raw/DNAlignments/Vochysiaceae/",i),
+                   format = "sequential",
+                   interleaved = FALSE, gap = "-",
+                   missing = "?", partition.file = NULL)
+}
+
+f <- list.files("data-raw/DNAlignments/Vochysiaceae")
+genes <- f[grepl("[.]nex", f)]
+Vochysiaceae <- list()
+for (i in genes) {
+  Vochysiaceae[[i]] <- ape::read.nexus.data(paste0("data-raw/DNAlignments/Vochysiaceae/",i))
+
+  #Vochysiaceae[[i]] <- phylotools::read.phylip(paste0("data-raw/DNAlignments/Vochysiaceae/",i))
+  # renaming the terminals
+  hitaxa <- gsub("(_[^_]+)_.*", "\\1", names(Vochysiaceae[[i]]))
+  taxa_temp <- sub(".*?_", "",  names(Vochysiaceae[[i]]))
+  taxa <- sub(".*?_", "",  taxa_temp)
+  names(Vochysiaceae[[i]]) <- paste(taxa, hitaxa, sep="_")
+  names(Vochysiaceae[[i]]) <- gsub("__", "_", names(Vochysiaceae[[i]]))
+}
+names(Vochysiaceae) <- gsub("[.].*", "", names(Vochysiaceae))
+
+# Adding dataset for tests
+usethis::use_data(Vochysiaceae, overwrite = TRUE)
